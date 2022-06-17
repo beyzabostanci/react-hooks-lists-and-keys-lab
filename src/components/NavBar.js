5087a233ffc6ec334e96d0f398eea061f36b8865
beyzabostanci <beyza_bostanc@hotmail.com>
2022-06-17 20:17:22 +0300

    completed

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,14 @@
 import React from "react";
-
+//Make sure each <a> element also gets a unique key prop
+//<a href="#home">home</a>
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const navLinks = links.map((link)=>(
+    <a key={link} href={"#" + link}>{link}
+    </a>
+  ))
+  return <nav>{navLinks}</nav>;
 }
 
 export default NavBar;
